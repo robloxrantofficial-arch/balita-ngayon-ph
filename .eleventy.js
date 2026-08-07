f416f5fe5fd5197166d78875b52e627892d847cb
robloxrantofficial-arch <robloxrantofficial@gmail.com>
2026-08-08 01:57:13 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = function(eleventyConfig) {
-  // Awtomatikong kopyahin ang CSS folder papunta sa build output
+  // 1. Awtomatikong kopyahin ang CSS folder papunta sa build output (Ang orihinal mong setting)
   eleventyConfig.addPassthroughCopy("src/assets/css");
 
+  // 2. PINALUWAG NA TEMPLATE ENGINES: Pinapahintulutan ang Eleventy na basahin ang HTML at mga Larawan sa loob ng Markdown files
   return {
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "_site",
