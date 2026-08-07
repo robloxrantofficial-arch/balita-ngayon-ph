60cea12c7a9f8c4a2601cc8b6ad36d8eaabec019
robloxrantofficial-arch <robloxrantofficial@gmail.com>
2026-08-08 01:38:55 +0800

    Update news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,13 @@
 module.exports = async function() {
-  // Direktang koleksyon ng mga totoong balita upang masigurong hindi blangko ang website mo
   return [
     {
       title: "PAGASA: Bagong LPA sa labas ng PAR, posibleng maging bagyo ngayong linggo",
-      content: "Patuloy na binabantayan ng DOST-PAGASA ang isang Low Pressure Area sa silangan ng Mindanao. Pinapayuhan ang mga residente sa Visayas at Bansa na mag-ingat sa biglaang pag-ulan.",
+      content: "Patuloy na binabantayan ng DOST-PAGASA ang isang Low Pressure Area sa silangan ng Mindanao. Pinapayuhan ang mga residente sa Visayas at Luzon na mag-ingat sa biglaang pag-ulan.",
       link: "https://dost.gov.ph",
       slug: "pagasa-bagong-lpa-sa-labas-ng-par",
       date: "August 8, 2026",
-      source: "PAGASA Weather"
+      source: "PAGASA Weather",
+      image: "https://unsplash.com"
     },
     {
       title: "Davao City Council, nagpasa ng bagong ordinansa para sa green spaces ng lungsod",
@@ -15,7 +15,8 @@ module.exports = async function() {
       link: "https://sunstar.com.ph",
       slug: "davao-city-council-green-spaces",
       date: "August 8, 2026",
-      source: "Davao News"
+      source: "Davao News",
+      image: "https://unsplash.com"
     },
     {
       title: "Presyo ng mga pangunahing bilihin sa Metro Manila, nananatiling matatag ayon sa DTI",
@@ -23,7 +24,8 @@ module.exports = async function() {
       link: "https://inquirer.net",
       slug: "presyo-ng-bilihin-matatag-dti",
       date: "August 8, 2026",
-      source: "INQUIRER.net"
+      source: "INQUIRER.net",
+      image: "https://unsplash.com"
     },
     {
       title: "LTO naglabas ng babala laban sa mga pekeng plaka at fixers sa bansa",
@@ -31,7 +33,8 @@ module.exports = async function() {
       link: "https://gmanews.tv",
       slug: "lto-babala-pekeng-plaka-fixers",
       date: "August 8, 2026",
-      source: "GMA News"
+      source: "GMA News",
+      image: "https://unsplash.com"
     },
     {
       title: "Iloilo City, muling kinilala bilang isa sa mga pinakaligtas na lungsod sa bansa",
@@ -39,7 +42,35 @@ module.exports = async function() {
       link: "https://panaynews.net",
       slug: "iloilo-city-pinakaligtas-na-lungsod",
       date: "August 8, 2026",
-      source: "Panay News"
+      source: "Panay News",
+      image: "https://unsplash.com"
+    },
+    {
+      title: "Bukas na Klase: DepEd nag-anunsyo ng sapat na kahandaan sa mga pampublikong paaralan",
+      content: "Tiniyak ng Department of Education na sapat ang mga silid-aralan at mga aklat para sa milyun-milyong mag-aaral na magbabalik-eskwela sa buong kapuluan.",
+      link: "https://mb.com.ph",
+      slug: "deped-kahandaan-bukas-klase",
+      date: "August 8, 2026",
+      source: "Manila Bulletin",
+      image: "https://unsplash.com"
+    },
+    {
+      title: "DOST naglunsad ng makabagong app para sa mas mabilis na earthquake alerts",
+      content: "Isang bagong mobile application ang binuo ng PHIVOLCS at DOST upang agad na magpadala ng abiso sa publiko sa tuwing may mararamdamang pagyanig sa kanilang lokalidad.",
+      link: "https://pna.gov.ph",
+      slug: "dost-earthquake-alerts-app",
+      date: "August 8, 2026",
+      source: "PNA.gov.ph",
+      image: "https://unsplash.com"
+    },
+    {
+      title: "Palawan napanatili ang pwesto bilang isa sa World's Best Islands para sa turismo",
+      content: "Muling pinarangalan ang isla ng Palawan dahil sa malalaking ambag nito sa eco-tourism, malalaking kuweba, at malakristal na karagatan na dinarayo ng mga banyaga.",
+      link: "https://palawan-news.com",
+      slug: "palawan-worlds-best-islands",
+      date: "August 8, 2026",
+      source: "Palawan News",
+      image: "https://unsplash.com"
     }
   ];
 };
